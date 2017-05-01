2b9860cb808fd61777d8830ab898424ea871b4b3
Ralf <ralf@stro-bel.de>
2017-05-01 22:01:05 +0200

    Initial commit

---
+++ 3-k8s-install.sh
@@ -0,0 +1,14 @@
+#Become root first sudo su -
+echo 'Updape'
+apt-get update 
+
+echo 'Install docker'
+apt-get -y install docker.io 
+
+echo 'Install kubelet, kubeadm, kubectl and kubernetes-cni'
+apt-get install -y apt-transport-https  
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -  
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list  
+deb http://apt.kubernetes.io/ kubernetes-xenial main  
+EOF  
+apt-get install -y kubelet kubeadm kubectl kubernetes-cni
---
+++ README.md
@@ -0,0 +1,2 @@
+Helpful snippets to create a Kubernetes Cluster using Pine64s
+Find the full tutorial here: http://blog.stro-bel.de/build-your-own-kubernetes-cluster-with-pine64/
