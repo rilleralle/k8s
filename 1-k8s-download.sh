85df990a4f74e3ce80c40d0f56f8a9660b5fa61d
rilleralle <rilleralle@users.noreply.github.com>
2017-05-08 13:14:34 +0200

    curl is available on Mac an Linux

--- 1-k8s-download.sh
+++ 1-k8s-download.sh
@@ -1,5 +1,5 @@
 echo '-----Download xubuntu-xenial-mate from pine64.org and extract-----'
-wget http://files.pine64.org/os/ubuntu/xubuntu-xenial-mate-20161215-longsleep-pine64-8GB.img.gz | gunzip
+curl "http://files.pine64.org/os/ubuntu/xubuntu-xenial-mate-20161215-longsleep-pine64-8GB.img.gz" -o "xubuntu-xenial-mate-20161215-longsleep-pine64-8GB.img.gz" | gunzip
 
 echo '-----Download Ubuntu Server for ARM64-----'
-wget http://cdimage.ubuntu.com/releases/16.04.2/release/ubuntu-16.04.2-server-arm64.iso
+curl "http://cdimage.ubuntu.com/releases/16.04.2/release/ubuntu-16.04.2-server-arm64.iso" -o "ubuntu-16.04.2-server-arm64.iso"
