b22a5a94fc89e35c67fd5ae493bef275d0a4228c
Ralf <rilleralle@users.noreply.github.com>
2017-07-13 15:57:23 +0000

    Call update

--- 3-k8s-install.sh
+++ 3-k8s-install.sh
@@ -10,8 +10,15 @@ apt-get -y install docker.io
 
 echo 'Install kubelet, kubeadm, kubectl and kubernetes-cni'
 apt-get install -y apt-transport-https  
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -  
-cat <<EOF >/etc/apt/sources.list.d/kubernetes.list  
-deb http://apt.kubernetes.io/ kubernetes-xenial main  
-EOF  
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
+echo 'Update'
+apt-get update 
+
+echo 'Upgrade'
+apt-get -y upgrade
+
 apt-get install -y kubelet kubeadm kubectl kubernetes-cni
