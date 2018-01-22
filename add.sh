53c81470671db126877e27b079b4d0d133692b06
Ralf <rilleralle@users.noreply.github.com>
2018-01-22 20:48:34 +0000

    Update add.sh

--- add.sh
+++ add.sh
@@ -1,1 +1,1 @@
-kubeadm join --token f27901.4ebd895b2977b1cf 192.168.11.1:6443 --discovery-token-ca-cert-hash sha256:533180b04bcc2298b68032517f22f3ba9c7ca1e1e9d34dc7ef9e119809a04480
+kubeadm join --token 56bbd1.88f4c202d90ab760 192.168.11.1:6443 --discovery-token-ca-cert-hash sha256:feb2786f5de029f819eef5d624b9724376872c076868312bf4b646068106b3a0
