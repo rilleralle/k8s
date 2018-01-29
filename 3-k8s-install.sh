1332bcae5e0977eee437455fd00fb3404e254e33
Ralf <rilleralle@users.noreply.github.com>
2018-01-29 16:46:50 +0100

    Update to version 1.9.2

--- 3-k8s-install.sh
+++ 3-k8s-install.sh
@@ -1,24 +1,14 @@
 #Become root first sudo su -
-echo 'Update'
-apt-get update 
-
-echo 'Upgrade'
-apt-get -y upgrade
-
 echo 'Install docker'
+apt-get update 
+apt-get -y upgrade
 apt-get -y install docker.io 
 
-echo 'Install kubelet, kubeadm, kubectl and kubernetes-cni'
-apt-get install -y apt-transport-https  
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb http://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-
-echo 'Update'
-apt-get update 
-
-echo 'Upgrade'
-apt-get -y upgrade
-
-apt-get install -y kubelet kubeadm kubectl kubernetes-cni
+echo 'Install kubelet, kubeadm and kubectl'
+apt-get update && apt-get install -y apt-transport-https  
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -  
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list  
+deb http://apt.kubernetes.io/ kubernetes-xenial main  
+EOF  
+apt-get update  
+apt-get install -y kubelet kubeadm kubectl 
