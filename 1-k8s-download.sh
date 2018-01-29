a85b3792ae6fbf6bca1f62b59a2a012db91acf7b
Ralf <rilleralle@users.noreply.github.com>
2018-01-29 10:26:22 +0100

    Update 1-k8s-download.sh

--- 1-k8s-download.sh
+++ 1-k8s-download.sh
@@ -1,5 +1,5 @@
 echo '-----Download xubuntu-xenial-mate from pine64.org and extract-----'
 curl "http://files.pine64.org/os/ubuntu/xubuntu-xenial-mate-20161215-longsleep-pine64-8GB.img.gz" -o "xubuntu-xenial-mate-20161215-longsleep-pine64-8GB.img.gz" | gunzip
 
-echo '-----Download Ubuntu Server for ARM64-----'
-curl "http://cdimage.ubuntu.com/releases/16.04.2/release/ubuntu-16.04.2-server-arm64.iso" -o "ubuntu-16.04.2-server-arm64.iso"
+echo '-----Download Xenial Base Image from pine64.org-----'
+curl "http://files.pine64.org/os/ubuntu/xenial-pine64-bspkernel-20161218-1.img.xz" -o "xenial-pine64-bspkernel-20161218-1.img.xz"
